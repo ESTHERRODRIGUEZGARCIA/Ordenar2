5e05a249bf4c679f62916ed348ced6be7f6a0da1
Esther  Rodríguez <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-03-09 09:40:58 +0000

    cambios varios

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-from ejercicios import ejercicio1
+variable = int(input("Seleccione que ejercicio desea ejecutar(1-3): "))
+if variable == 1:
+    from ejercicios import ejercicio1
+elif variable == 2:
+    from ejercicios import ejercicio2
+elif variable == 3:
+    from ejercicios import ejercicio3
+else:
+    print("Introduzca valores entre 1 y 3.")
